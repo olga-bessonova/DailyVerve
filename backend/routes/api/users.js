f1ce7589be96f6f6de0d7e4a9a0c4d2c43592a32
Olga <olga.sleepless@gmail.com>
2023-08-06 12:28:25 -0400

    Update backend

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello world!');
+  res.json({
+    message: "GET /api/users"
+  });
 });
 
 module.exports = router;

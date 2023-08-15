b2926938122382f4037bbdb1a8fc9243402037a8
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-08-15 14:41:13 -0400

    Add Redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from './store/store';
+
+let store = configureStore({});
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );

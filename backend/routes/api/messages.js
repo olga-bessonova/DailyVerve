d6120021864a8419e9097e82f997375887a5b7d2
Olga <olga.sleepless@gmail.com>
2023-09-04 22:08:59 -0400

    Change email params

--- backend/routes/api/messages.js
+++ backend/routes/api/messages.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const OpenAI = require('openai');
 const Message = mongoose.model('Message');
 const sgMail = require('@sendgrid/mail')
+const schedule = require('node-schedule');
 const { requireUser } = require('../../config/passport');
 const { openAiApiKey } = require('../../config/keys');
 
@@ -40,26 +41,55 @@ router.post('/', requireUser, async (req, res, next) => {
   }
 });
 
-// POST /api/messages/email
+// // POST /api/messages/email
+
+// router.post('/email', async (req, res, next) => {
+//     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+//     const msg = {
+//       to: 'teamsigndesign@gmail.com', 
+//       from: 'info.daily.verve@gmail.com', 
+//       send_at: 1693877295,
+//       subject: 'Sending with SendGrid is Fun',
+//       text: 'Why did the scarecrow win an award?Because he was outstanding in his field!',
+//       html: "<strong> What did one wall say to the other wall? I'll meet you at the corner! </strong>",
+//     }
+//     sgMail
+//       .send(msg)
+//       .then(() => {
+//         console.log('Email sent')
+//         res.send(msg)
+//       })
+//       .catch((error) => {
+//         console.error(error)
+//       })
+// });
+const sendEmail = () => {
+  const msg = {
+    to: 'olga.sleepless@gmail.com',
+    from: 'info.daily.verve@gmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    html: "<strong>What did one wall say to the other wall? I'll meet you at the corner!</strong>",
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+// Schedule the email to be sent every day at 9:40 PM EST
+const cronExpression = '40 21 * * *'; // EST is 5 hours behind UTC
+const scheduledJob = schedule.scheduleJob(cronExpression, () => {
+  sendEmail();
+});
 
 router.post('/email', async (req, res, next) => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-      to: 'olga.sleepless@gmail.com', // Change to your recipient
-      from: 'info.daily.verve@gmail.com', // Change to your verified sender
-      subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log('Email sent')
-        res.send(msg)
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+  res.send('Email will be sent every day at 9:40 PM EST.');
 });
 
 

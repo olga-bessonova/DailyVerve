7dad95581beb86b73dc8cf6b493079703b013a2f
Olga <olga.sleepless@gmail.com>
2023-09-07 21:21:19 -0400

    Update Navigation

---
+++ frontend/src/components/DropDown/DropDown.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect} from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../../store/session';
+
+import './DropDown.css';
+
+const DropDown = ({ user }) => {
+  const [showCreateTripModal, setShowCreateTripModal] = useState(false);
+  const dispatch = useDispatch();
+
+  const toggleMenu = () => {
+    setMenu(open => !open);21``
+  };
+
+  const [menu, setMenu] = useState(false);
+
+  useEffect(() => {
+    if (!menu) return;
+
+    const closeMenu = () => {
+      setMenu(false);
+    };
+    document.addEventListener('click', closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
+  }, [menu, showCreateTripModal]);
+
+  const logoutUser = e => {
+    e.preventDefault();
+    dispatch(logout());
+}
+
+  return (
+    <>
+      <button className='profile_button' onClick={toggleMenu}>
+        <img className="profile_image" src={user.profileImageUrl} alt=""/> 
+        <span className='drop_down_username'>{user.username}</span>
+      </button>
+
+      {menu && (
+        <ul className="profile_dropdown" onClick={() => setMenu(false)}>
+          {/* <li className='profile_dropdown_navlink' id="profile_dropdown_navlink_username">{user.username}</li> */}
+          <li id="profile_dropdown_navlink_username">{user.username}</li>
+          <li className='dropdown_divider'></li>
+          <li className='profile_dropdown_navlink'>
+            <Link to={'/profile'}>
+              <span className='profile_dropdown_span' id="your_trips_button">your trips</span>
+            </Link>
+          </li>
+
+          <li className='profile_dropdown_navlink'> <button id="create_trip_button" onClick={()=> setShowCreateTripModal(true)}>create a trip</button></li>
+
+          <li className='profile_dropdown_navlink'> 
+            <Link to={'/'}> 
+              <button id="logout_button" onClick={logoutUser}>logout</button>
+            </Link>
+          </li>
+
+        </ul>
+      )}
+
+    </>
+  );
+}
+
+export default DropDown;
---
+++ frontend/src/components/SessionForms/LoginFormModal.js
@@ -0,0 +1,96 @@
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import './FormModal.css';
+
+
+const LoginFormModal = (props) => {
+  const user = useSelector(state => state.session.user); 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const errors = useSelector(state => state.errors.session);
+  const {setShowLoginModal} = props;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(sessionActions.clearSessionErrors());
+    };
+  }, [dispatch]);
+
+  const update = (field) => {
+    const setState = field === 'email' ? setEmail : setPassword;
+    return e => setState(e.currentTarget.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.login({ email, password }))
+      .then(res => {
+        if (res.currentUser)  setShowLoginModal(false)
+    });
+  };
+
+  const handleDemo = (e) => {
+    dispatch(sessionActions.login({ 
+      email: 'demo@user.io', 
+      password: 'password' 
+    }))
+    .then(res => {
+      if (res.currentUser)  setShowLoginModal(false)
+    }); 
+  };
+
+  if (user) {
+    return <Redirect to="/profile" />;
+  };
+
+  return (
+
+    <div className='login-modal'>
+      <div onClick={() => setShowLoginModal(false)} className="close-button">
+        <i className="fa-solid fa-x"></i>
+      </div>
+
+      <header className="login-header">
+        <div className="login-header-text">Log in</div>
+      </header> 
+
+      <form className="login-form" onSubmit={handleSubmit}>
+        
+        <div className="errors">{errors?.email}</div>
+        
+        <div className="email_div">
+          <input className="email-login-input" type="text"
+            value={email}
+            onChange={update('email')}
+            placeholder="Email"
+          />
+        </div>
+
+        
+        <div className="password-div">
+          <input className="password-login-input" type="password"
+            value={password}
+            onChange={update('password')}
+            placeholder="Password"
+          />
+        </div>
+        <div className="errors">{errors?.password}</div>
+        <button type="submit" className="continue_button" >Continue</button>
+
+    </form>
+
+        <div id="demo-button-div">
+          <button className="demo_button" onClick={handleDemo}>Continue with Demo User</button>
+        </div>  
+      
+
+    </div>
+
+    
+  );
+}
+
+export default LoginFormModal;
